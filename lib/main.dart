54d334ab506046811472159a5c446bd7a2d6b4a1
Brayden Halliday <brayden@clickspace.com>
2022-03-03 17:53:09 -0700

    provider test submission

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,49 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 /// Represenst a Cart Item. Has <int>`id`, <String>`name`, <int>`quantity`
-class CartItem {}
-
+class CartItem {
+  int id;
+  String name;
+  int quantity;
+  CartItem(this.id, this.name, this.quantity);
+}
 /// Manages a cart. Implements ChangeNotifier
 class CartState with ChangeNotifier {
   List<CartItem> _products = [];
-
   CartState();
-
   /// The number of individual items in the cart. That is, all cart items' quantities.
-  int get totalCartItems => 0; // TODO: return actual cart volume.
-
+  int get totalCartItems => _products.fold(0, (totalQuantity, product) => totalQuantity + product.quantity);
   /// The list of CartItems in the cart
   List<CartItem> get products => _products;
-
   /// Clears the cart. Notifies any consumers.
-  void clearCart() {}
-
+  void clearCart() {
+    _products = [];
+    notifyListeners();
+  }
   /// Adds a new CartItem to the cart. Notifies any consumers.
-  void addToCart({required CartItem item}) {}
-
-  /// Updates the quantity of the Cart item with this id. Notifies any consumers.
-  void updateQuantity({required int id, required int newQty}) {}
+  void addToCart({required CartItem item}) {
+    _products.add(item);
+    notifyListeners();
+  }
+  /// Updates the quantity of the Cart item with this id. Notifies any consumers
+  void updateQuantity({required int id, required int newQty}) {
+    _products.forEach((product) {product.id == id ? product.quantity = newQty: "" ;});
+    notifyListeners();
+  }
+  void removeProduct({required int index}) {
+    _products.removeAt(index);
+    notifyListeners();
+  }
+  returnShoutOut({required int numberOfCats}) {
+    List catComments = ["you should pick out some cats","only 1 cat, you can do better!!", "2 is always better than 1", "3 is a party!", " wow! looks like you're taking all the cat!"];
+    if (numberOfCats == 0) {
+      return(catComments[0]);
+    } else if(numberOfCats < 4) {
+      return(catComments[numberOfCats]);
+    } else return catComments[4];
+  }
 }
 
 void main() {
@@ -47,6 +65,7 @@ class MyCartApp extends StatelessWidget {
             children: [
               ListOfCartItems(),
               CartSummary(),
+              CartComments(),
               CartControls(),
             ],
           ),
@@ -64,15 +83,13 @@ class CartControls extends StatelessWidget {
     int nextCartItemId = Random().nextInt(10000);
     String nextCartItemName = 'A cart item';
     int nextCartItemQuantity = 1;
-
-    CartItem
-        item; // Actually use the CartItem constructor to assign id, name and quantity
-
-    // TODO: Get the cart current state through Provider. Add this cart item to cart.
+    CartItem item = new CartItem(nextCartItemId, nextCartItemName, nextCartItemQuantity); // Actually use the CartItem constructor to assign id, name and quantity
+    Provider.of<CartState>(context,listen: false).addToCart(item: item);
   }
-
   /// Handle clear cart pressed. Should clear the cart
-  void _clearCartPressed(BuildContext context) {}
+  void _clearCartPressed(BuildContext context) {
+    Provider.of<CartState>(context,listen: false).clearCart();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -82,14 +99,12 @@ class CartControls extends StatelessWidget {
         _addItemPressed(context);
       },
     );
-
     final Widget clearCartWidget = TextButton(
       child: Text('Clear Cart'),
       onPressed: () {
         _clearCartPressed(context);
       },
     );
-
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -102,37 +117,49 @@ class CartControls extends StatelessWidget {
 
 class ListOfCartItems extends StatelessWidget {
   /// Handles adding 1 to the current cart item quantity.
-  void _incrementQuantity(context, int id, int delta) {}
-
+  void _incrementQuantity(context, int id, int delta) {
+    int activeProductindex = Provider.of<CartState>(context, listen: false).products.indexWhere((product) => product.id == id);
+    CartItem activeProduct = Provider.of<CartState>(context, listen: false).products[activeProductindex];
+    Provider.of<CartState>(context, listen: false).updateQuantity(id: id, newQty: activeProduct.quantity + delta);
+  }
   /// Handles removing 1 to the current cart item quantity.
-  /// Don't forget: we can't have negative numbers of an item in the cart
-  void _decrementQuantity(context, int id, int delta) {}
-
+  void _decrementQuantity(context, int id, int delta) {
+    int activeProductindex = Provider.of<CartState>(context, listen: false).products.indexWhere((product) => product.id == id);
+    CartItem activeProduct = Provider.of<CartState>(context, listen: false).products[activeProductindex];
+    if(activeProduct.quantity - delta > 0) {
+      Provider.of<CartState>(context, listen: false).updateQuantity(id: id, newQty: activeProduct.quantity - delta);
+    } else {Provider.of<CartState>(context, listen: false).removeProduct(index: activeProductindex);
+    };
+  }
   @override
   Widget build(BuildContext context) {
     return Consumer<CartState>(
         builder: (BuildContext context, CartState cart, Widget? child) {
-      if (cart.totalCartItems == 0) {
-        // TODO: return a Widget that tells us there are no items in the cart
-      }
-
-      return Column(children: [
-        ...cart.products.map(
-          (c) => Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                // TODO: Widget for the line item name AND current quantity, eg "Item name x 4".
-                //  Current quantity should update whenever a change occurs.
-                // TODO: Button to handle incrementing cart quantity. Handler is above.
-                // TODO: Button to handle decrementing cart quantity. Handler is above.
-              ],
+          final ButtonStyle style =
+          ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 10));
+          if (cart.totalCartItems == 0) {
+            return Text("there are no items in the cart");
+          }
+          return Column(children: [
+            ...cart.products.map(
+                  (c) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("${c.name} x ${c.quantity}"),
+                    Image.network('https://cataas.com/cat',
+                      height: 50, width: 50,
+                    ),
+                    //  Current quantity should update whenever a change occurs.
+                    ElevatedButton(onPressed: () =>_incrementQuantity(context, c.id, 1), child: Text("+"), style: style),
+                    ElevatedButton(onPressed: () =>_decrementQuantity(context, c.id, 1), child: Text("-"), style: style),
+                  ],
+                ),
+              ),
             ),
-          ),
-        ),
-      ]);
-    });
+          ]);
+        });
   }
 }
 
@@ -142,6 +169,18 @@ class CartSummary extends StatelessWidget {
     return Consumer<CartState>(
       builder: (BuildContext context, CartState cart, Widget? child) {
         return Text("Total items: ${cart.totalCartItems}");
+
+      },
+    );
+  }
+}
+
+class CartComments extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CartState>(
+      builder: (BuildContext context, CartState cart, Widget? child) {
+        return Text("${Provider.of<CartState>(context, listen: false).returnShoutOut(numberOfCats: cart.totalCartItems)}");
       },
     );
   }
